8789b2d2c611a3482dd05b063aa4b3fbb79974cd
jacomar <jacomar304@hotmail.com>
2023-12-17 18:39:22 +0100

    Revert "Commit 1: Inicialización del proyecto."
    This reverts commit 343b8cf239493e0788621c14534cb82e35a0f019.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,5 @@
 package org.example
 
 fun main() {
-    println("Benvingut al programa de càlcul del valor de mercat de la Volkswagen Grand California")
-
-    // Demanar al usuari el model de la Volkswagen i l'estat dels pneumàtics
-    val model = llegirModel()
-    val kmNeumaticos = llegirKmNeumaticos()
-
-    // Calcular el preu base i el preu actual
-    val preuBase = calcularPreuBase(model)
-    val preuActual = calcularPreuActual(preuBase, kmNeumaticos)
-
-    // Mostrar els resultats a l'usuari
-    println("Preu base original de la furgo: $preuBase €")
-    println("Preu actual de la furgo: $preuActual €")
+    println("Hello World!")
 }
